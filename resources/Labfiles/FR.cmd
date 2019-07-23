86efc18dcb01ab477a268ec6dc1984c814eff544
onofflink <onofftony@gmail.com>
2019-07-23 10:40:07 +0900

    more of other files

---
+++ resources/Labfiles/FR.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo *=============================================================================*
+echo * SOLDESK IT Academy Cisco Training                                           *
+echo * 								              *
+echo * Frame-Relay Switch Configure                                                *
+echo * 								              *
+echo * CCNP LAB Guide By Kim Jung Woo                                              *
+echo * 								              *
+echo * CCNP LAB Topology/Preconfig : cafe.naver.com/itwillcisco                      *
+echo *=============================================================================*
+cd tmp
+"..\bin\dynagen\dynagen.exe" ..\net\fr.net
---
+++ resources/Labfiles/MPLS.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo *=============================================================================*
+echo * SOLDESK IT Academy Cisco Training                                           *
+echo * 								              *
+echo * MPLS VPN LAB                                                                *
+echo * 								              *
+echo * CCNP LAB Guide By Kim Jung Woo                                              *
+echo * 								              *
+echo * CCNP LAB Topology/Preconfig : cafe.naver.com/itwillcisco                    *
+echo *=============================================================================*
+cd tmp
+"..\bin\dynagen\dynagen.exe" ..\net\mpls.net
